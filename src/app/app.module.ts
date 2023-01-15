40c9a7b38c5b33e7f65abdc6d3193dee9b2473ba
Sergey Martyshenko <limonpudding@gmail.com>
2023-01-15 11:04:45 +0500

    #2 Добавлены основные компоненты приложения, настроен роутинг

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { GameComponent } from './game/game.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    GameComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
